cf01013fec04393278d79a8c4ec3408566fb9e0f
reecewayt <reecewayt96@gmail.com>
2024-04-19 18:17:35 -0700

    Adding new timing frequency feature to script

--- tests/reeces-test/led-blink/runmgmt.c
+++ tests/reeces-test/led-blink/runmgmt.c
@@ -20,7 +20,7 @@ int do_run_mgmt(runState *s) {
 void run_mgmt(runState *s) {
      spikeCountCx += SPIKE_COUNT[(s->time_step-1)&3][0x20+probe_id]; // This macro is used to read the lakemont spike counter
      SPIKE_COUNT[(s->time_step-1)&3][0x20+probe_id] = 0;    // Lakemont spike counters need to be cleared after reading to prevent overflow  
-     printf("Spike received %d...\n", spikeCountCx);
-     printf("Time step, %d", s->time_step);
+     //printf("Spike received %d...\n", spikeCountCx);
+     printf("Time step, %d\n", s->time_step);
      writeChannel(channelID, &spikeCountCx, 1);             // Write the spike counter value back to the channel
 }
--- tests/reeces-test/led-blink/snip-for-reading-lakemont-spike-count.py
+++ tests/reeces-test/led-blink/snip-for-reading-lakemont-spike-count.py
@@ -6,12 +6,27 @@
 # Test Purpose: This is a preliminary test program to determine speed of snip send/recv channels from superhost to
 # x86 chips on the KB (host). 
 
-
+import sys
 import os
+
+# Gets the directory where this program is located
+this_program_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Constructs and normalizes the path to the benchmark_kit directory
+benchmark_kit_path = os.path.normpath(os.path.join(this_program_dir, '../benchmark_kit'))
+
+# Add the benchmark kit directory to sys.path
+sys.path.append(benchmark_kit_path)
+
 import nxsdk.api.n2a as nx
 import numpy as np
 import matplotlib.pyplot as plt
 from nxsdk.graph.monitor.probes import *
+from benchmark_kit.perf_wrappers import timeit
+
+@timeit
+def read_spike_counter(spikeCntrChannel):
+    return spikeCntrChannel.read(1)
 
 if __name__ == '__main__':
 
@@ -57,19 +72,64 @@ if __name__ == '__main__':
                                      funcName = "run_mgmt",
                                      guardName = "do_run_mgmt",
                                      phase = "mgmt")
+    
+    # Defining a process which will run during spiking phase
+    # This snip will run on chip 1 lmt 0
+    '''
+    spikingProcess = board.createProcess(
+                                     name="spikingProcess",
+                                     cFilePath= includeDir + "/spiking.c",
+                                     includeDir=includeDir,
+                                     funcName="run_spiking",
+                                     guardName="do_spiking",
+                                     phase="spiking",
+                                     chipId=1,
+                                     lmtId=0
+                                    )
+    '''
+    
                           
     # Create a channel named spikeCntr to get the spikes count information from Lakemont
-    spikeCntrChannel = board.createChannel(b'nxspkcntr', "int", 100)
+    spikeCntrChannel = board.createChannel(b'nxspkcntr', "int", 1)
+
+    ## TODO: implement later sendSpikeChannel = board.createChannel(b'nxsendspk', "int", 1)
 
     # Connecting spikeCntr from runMgmtProcess to SuperHost which is receiving spike count in the channel
     spikeCntrChannel.connect(runMgmtProcess, None)
 
+    ##sendSpikeChannel.connect(None, spikingProcess)
 
     board.run(100, aSync=True)
     # print(spikeCntrChannel.read(100)) doesn't work in real time, must read whole buffer otherwise stalls???
-    spikeCntr = []
-    spikeCntr.append(spikeCntrChannel.read(100))
-    print(spikeCntr)
+    steps = 0
+    execution_times = []
+    
+    for spike in spikeTimes0:
+        # Call the function and capture both return values
+        curr_count, elapsed_time = read_spike_counter(spikeCntrChannel)
+
+        # Append the elapsed time to the execution_times list
+        execution_times.append(elapsed_time)
+    
+        print(curr_count)  # Print the current count
+        prev_count = curr_count  # Update the previous count
+        steps += 1  # Increment the step count
+
+    #curr_count = spikeCntrChannel.read(1)    
+    print(steps)
+    #spikeCntr = []
+    #spikeCntr.append(spikeCntrChannel.read(100))
+    #print(spikeCntr)
 
     board.finishRun()
     board.disconnect()
+
+
+    # Convert execution times to frequencies (in Hz)
+    frequencies = [1 / time for time in execution_times if time > 0]  # Avoid division by zero
+
+    # Compute the average frequency
+    average_frequency = sum(frequencies) / len(frequencies) if frequencies else 0
+
+    print("Frequencies: ", frequencies)
+    print("Average Frequency: {:.2f} Hz".format(average_frequency))
