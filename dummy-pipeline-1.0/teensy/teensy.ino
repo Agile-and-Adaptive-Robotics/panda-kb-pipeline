c76798c56b7daf3915b2387f370a4705b5516037
Reece Wayt <reecewayt96@gmail.com>
2024-07-01 11:52:46 -0700

    Updating test code for UART connect

--- dummy-pipeline-1.0/teensy/teensy.ino
+++ dummy-pipeline-1.0/teensy/teensy.ino
@@ -4,49 +4,46 @@
   Notes on Teensys: 
    -  All serial ports on Teensy 4.0 and 4.1 have 4 byte transmit and receive FIFOs. 
    -  At very high baud rates, even small software delays can cause a receive buffer to overflow and data will be lost. 
-
+   -  Serial 2 uses pins: 7 (RX) and 8 (TX) on Teensy 4.0 and 4.1
   This is a very simple UART test program
 */
+
+//**Teensy Code**
+
 #define MAX_BAUD_RATE 1000000 //1 Mbps is max speed
-#define RTS_PIN 7 // Ready to Send (Output)
-#define CTS_PIN 8 // Clear to Send (Input)
 #define BUFFER_SIZE 50
 
 volatile byte buf[BUFFER_SIZE];
 volatile byte pos = 0;
 
 void setup() {
-  Serial1.begin(MAX_BAUD_RATE);  // Initialize UART communication at 1 Mbps
-  pinMode(RTS_PIN, INPUT);
-  pinMode(CTS_PIN, OUTPUT);
-
+  Serial2.begin(MAX_BAUD_RATE);  // Initialize UART communication at 1 Mbps
   Serial.begin(115200);  // For debugging
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
 }
 
 void loop() {
   // Check if Serial1 Rx buffer has data
-  if (Serial1.available() > 0) {
-    // Check if Serial buffer has space
-    if (Serial.availableForWrite() > 0) {
-      digitalWrite(CTS_PIN, HIGH);  // Signal clear to send
-
-      // Read incoming binary data
-      while (Serial1.available() > 0 && pos < BUFFER_SIZE) {
-        buf[pos++] = Serial1.read();
-      }
-
-      // Print received binary data for debugging
-      Serial.print("Received: ");
-      for (int i = 0; i < pos; i++) {
-        Serial.print(buf[i], HEX);
-        Serial.print(" ");
-      }
-      Serial.println();
-
-      pos = 0;  // Reset buffer position
-
-      digitalWrite(CTS_PIN, LOW);  // Signal not clear to send
+  if (Serial2.available() >= 4) {
+    byte receivedData[4];
+    for (int i = 0; i < 4; i++){
+      receivedData[i] = Serial2.read();
     }
+
+    Serial.print("Received data: ");
+    for (int i = 0; i < 4; i++) {
+      Serial.print(receivedData[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+
+    byte responseData[] = {0xA1, 0xA2, 0xA3, 0xA4};
+    Serial.write(responseData, sizeof(responseData));
+
+    Serial.println("Response Sent");
+
   }
+  delay(100); 
 }
 
